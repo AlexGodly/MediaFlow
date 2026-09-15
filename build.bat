3846bcf10e88df8fc024e75586e15b3f7b1db860
Fucking Psycho <90983397+AlexGodly@users.noreply.github.com>
2026-09-15 19:49:56 +0100

    Add files via upload

--- build.bat
+++ build.bat
@@ -1,5 +1,5 @@
 @echo off
-REM Build MediaFlow 20.0.0 for Windows
+REM Build MediaFlow 21.0.0 for Windows
 echo Building MediaFlow...
 where node >nul 2>nul || (echo Node.js is required: https://nodejs.org & pause & exit /b 1)
 call npm install || (pause & exit /b 1)
